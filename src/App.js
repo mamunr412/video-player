614e4641e60d230b02ac94a6615b4c60f224d792
unknown <mamun.m360ict@gmail.com>
2022-08-17 16:07:05 +0600

    video player add

--- src/App.js
+++ src/App.js
@@ -1,71 +1,52 @@
-
 import './App.css';
 import { useEffect, useState } from 'react';
-import { ReactVideoPlayer } from 'video-player-for-react'
-import 'video-player-for-react/dist/index.css'
-import GoogleMapReact from 'google-map-react';
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+import { ReactVideoPlayer } from 'video-player-for-react';
+import 'video-player-for-react/dist/index.css';
+// import Map from './page/Map';
 
 function App() {
-  const [video,setVidedo]=useState([])
-  const [singleVideo,setSingleVideo]=useState('')
+  const [video, setVidedo] = useState([]);
+  const [singleVideo, setSingleVideo] = useState('');
 
-  useEffect(()=>{
-    fetch(`https://care-box-backend.herokuapp.com/api/v1/applicant_test/get_video_link/`)
-    .then(res=>res.json()).then(data=>{
-      setVidedo(data)
-    })
-  },[])
+  useEffect(() => {
+    fetch(
+      `https://care-box-backend.herokuapp.com/api/v1/applicant_test/get_video_link/`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setVidedo(data);
+      });
+  }, []);
 
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627
-    },
-    zoom: 11
-  };
   return (
-    <div className="App">
-      
-<div>
-<ReactVideoPlayer
-      width='928px'
-      url={singleVideo}
-      type='video/mp4'
-      poster='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg'
-      
-    />
-</div>
-<div>
- {video.map(v=>{
-  console.log(v)
-  return <div key={v.id}>
-    <ul>
-      <li onClick={()=>setSingleVideo(v.link)} style={{ cursor:'pointer'}}>{v.link}</li>
-    </ul>
-  </div>
- })} 
-</div>
-
-
-<div>
-<div style={{ height: '100vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text="My Marker"
+    <div className='App' style={{ marginLeft: '20%' }}>
+      <div>
+        <ReactVideoPlayer
+          width='928px'
+          url={singleVideo}
+          type='video/mp4'
+          poster='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg'
         />
-      </GoogleMapReact>
-    </div>
-</div>
+      </div>
+      <div>
+        {video.map((v) => {
+          console.log(v);
+          return (
+            <div key={v.id}>
+              <ul>
+                <li
+                  onClick={() => setSingleVideo(v.link)}
+                  style={{ cursor: 'pointer' }}
+                >
+                  {v.link}
+                </li>
+              </ul>
+            </div>
+          );
+        })}
+      </div>
 
-
+      <div>{/* <Map /> */}</div>
     </div>
   );
 }
---
+++ src/page/Map.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/* Only lat and lng are required for marker objects.*/
+
+const Map = () => {
+  return (
+    <div style={style}>
+      {/* <GMap bootstrapURLKeys={keys} center={center} markers={markers} /> */}
+    </div>
+  );
+};
+
+export default Map;
